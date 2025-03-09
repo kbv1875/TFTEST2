b4d6833bc9ec1fe78eafc11233fc4a1e104b0c38
karthik murali <karthikmurali123@gmail.com>
2025-03-10 01:20:18 +0530

    hehe

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
-module "old-r2_bucket" {
-  source = "./old-module"
+module "r2_bucket" {
+  source = "./modules/account/r2_bucket"
   cloudflare_account_id = var.cloudflare_account_id
   r2_location = var.r2_location
 }
