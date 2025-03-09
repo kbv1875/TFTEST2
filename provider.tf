6f9060e074ee7e90d8ed6f3e7936bc0880cb8864
karthik murali <karthikmurali123@gmail.com>
2025-03-10 00:48:47 +0530

    first

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.7.5"
+  required_providers {
+    time = {
+      source  = "hashicorp/time"
+      version = "0.11.1"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "4.46.0"
+    }
+  }
+}
+
+provider "cloudflare" {
+  api_token = var.cloudflare_api_token
+  
+}
+
---
+++ r2.tf
@@ -0,0 +1,5 @@
+resource "cloudflare_r2_bucket" "example_r2_bucket6" {
+  account_id = var.cloudflare_account_id
+  name = "tf-kbv-bucket-66"
+  location = var.r2_location
+}
