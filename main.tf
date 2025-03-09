291585b722b6fa2640362d90a21d359e43394ba2
karthik murali <karthikmurali123@gmail.com>
2025-03-10 01:16:51 +0530

    xxx

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ module "old-r2_bucket" {
   r2_location = var.r2_location
 }
 
-module "new-buckets" {
-  source = "./new-buckets"
-  cloudflare_account_id = var.cloudflare_account_id
-  r2_location = var.r2_location
+resource "cloudflare_r2_bucket" "example_r2_bucket5" {
+  account_id = var.cloudflare_account_id
+  name = "tf-kbv-bucket-55"
+  location = var.r2_location
 }
--- new-buckets/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "r2_location" {
-  description = "The location of the R2 bucket"
-  type        = string
-}
-
-variable "cloudflare_account_id" {
-  description = "Cloudflare Account ID"
-  type        = string
-}
